973af79f66c11eea60727fbda79cc88131a27fc5
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2023-12-19 11:14:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function toCase(text) {
   // write your code here
+	let str=text.toString();
+	if(str.length===0) return "-";
+	let res="";
+	res+=str.toLowerCase();
+	res+='-';
+	res+=str.toUpperCase();
+	return res;
 }
-
 // DO not change the code below
 
 const text = prompt("Enter text:");
